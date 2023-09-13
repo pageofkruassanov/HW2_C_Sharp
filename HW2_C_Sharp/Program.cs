aa31baebdb27d17680a07bcfdb2aaaa3e89e3535
Pavlo <pkhasanov@gmail.com>
2023-09-13 23:33:27 +0300

    Add project files.

---
+++ HW2_C_Sharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 4:
+            Replacement replacement = new Replacement();
+            replacement.SetNums(744839);
+            replacement.ChangePlaces(3, 6);
+            Console.WriteLine(replacement.GetNums() + "\n\n\n");
+
+            // Task 5:
+            Console.WriteLine("Введите дату в формате день/месяц/год (например, 30/12/2023):");
+            string tempDate = Console.ReadLine();
+
+            if (DateTime.TryParseExact(tempDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+            {
+                string dayWeek = date.ToString("dddd", CultureInfo.GetCultureInfo("ru-RU"));
+                Console.WriteLine($"День недели: {dayWeek}");
+
+                string month = date.ToString("MMMM", CultureInfo.GetCultureInfo("ru-RU"));
+                Console.WriteLine($"Месяц: {month}");
+            }
+            else
+            {
+                Console.WriteLine("Error/ Task 5");
+            }
+            Console.ReadLine();
+
+            // Task 7:
+            int start;
+            int finish;
+            Console.Write("Введите начальное число диапазона: ");
+            start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите конечное число диапазона: ");
+            finish = Convert.ToInt32(Console.ReadLine());
+            if(start > finish)
+            {
+                int tempNum = start;
+                start = finish;
+                finish = tempNum;
+            }
+            Console.WriteLine($"Все парные числа в диапазоне от {start} до {finish}: ");
+            while(start < finish)
+            {
+                if (start % 2 == 0)
+                {
+                    Console.Write(start + " ");
+                }
+                start++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HW2_C_Sharp/Replacement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_C_Sharp
+{
+    public class Replacement
+    {
+        private int _num;
+        private int _SIZE = 6;
+        public void SetNums(int num)
+        {
+            if (num < 100000 || num > 999999)
+                throw new Exception("Error/ SetNums");
+            else
+            {
+                _num = num;
+            }
+
+        }
+        public int GetNums()
+        {
+            return _num;
+        }
+        public void ChangePlaces(int firstIndex, int secondIndex)
+        {
+            if (firstIndex < 1 || firstIndex > 6 || secondIndex < 1 || secondIndex > 6)
+                throw new Exception("Error / ChangePlaces");
+            string numberString = _num.ToString();
+            char[] charArray = numberString.ToCharArray();
+
+            char temp = charArray[firstIndex - 1];
+            charArray[firstIndex - 1] = charArray[secondIndex - 1];
+            charArray[secondIndex - 1] = temp;
+
+            _num = int.Parse(new string(charArray));
+
+        }
+    }
+}
